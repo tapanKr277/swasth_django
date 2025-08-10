1e9be974b949ad15874b6b8e09a36a2d523599f3
AlexCrew007 <tapankumar07081998@gmail.com>
2025-08-10 23:34:21 +0530

    rollback added

--- backend/custom_user/views.py
+++ backend/custom_user/views.py
@@ -1,19 +1,25 @@
 from rest_framework.views import APIView
-from .serializers import CreateDoctroSerializer, CreatePatientSerializer, CreateDoctorSerializer
+from .serializers import CreatePatientSerializer, CreateDoctorSerializer
 from patient.serializers import ListAllPatientSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from patient.models import Patient
-from django.contrib.auth.models import Group, Permission
+from django.db import transaction
 
 class CreatePatientUserView(APIView):
+    @transaction.atomic
     def post(self, request):
-        serialized_data = CreatePatientSerializer(data=request.data, many=False)
-        if serialized_data.is_valid():
-            serialized_data.save()
-            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serialized_data = CreatePatientSerializer(data=request.data, many=False)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+            else:
+                transaction.set_rollback(True)
+                return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            transaction.set_rollback(True)
+            return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class ListAllPatientView(APIView):
     def get(self, request):
@@ -22,11 +28,17 @@ class ListAllPatientView(APIView):
         return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 class CreateDoctorUserView(APIView):
+    @transaction.atomic
     def post(self, request):
-        doctor_data = request.data
-        serialized_data = CreateDoctorSerializer(data=doctor_data, many=False)
-        if serialized_data.is_valid():
-            serialized_data.save()
-            return Response(serialized_data.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            doctor_data = request.data
+            serialized_data = CreateDoctorSerializer(data=doctor_data, many=False)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return Response(serialized_data.data, status=status.HTTP_201_CREATED)
+            else:
+                transaction.set_rollback(True)
+                return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            transaction.set_rollback(True)
+            return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
